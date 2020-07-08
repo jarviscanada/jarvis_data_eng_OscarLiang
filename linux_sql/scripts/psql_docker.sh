44c5b28475eb0d8ab6fea4518b3e28be54f87789
Oscar Liang <sam.oscar.liang@gmail.com>
2020-07-08 17:46:55 +0000

    Completed create and invalid cases in psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -6,12 +6,32 @@ action=$1
 db_user=$2
 db_pass=$3
 
+# Usage statement
+usage="Usage: ./scripts/psql_docker.sh start|stop|create [db_username][db_password]"
+
 # If docker is not running, start docker
 systemctl status docker || systemctl start docker
 
+# Check if docker container exists (value is 2 if exists)
+container_exists=$(docker container ls -a -f name=jrvs-psql | wc -l)
+
+# Parse action
 case $action in
     'create')
-        echo 'create'
+        if [ "$container_exists" = "2" ]; then
+            echo "Error: container already exists. $usage" >&2
+            exit 1
+        fi
+
+        if [ "$db_pass" = "" ]; then
+            echo "Error: missing username and/or password. $usage" >&2
+            exit 1
+        fi
+
+        # If no errors, create docker container
+        docker volume create pgdata
+        docker run --name jrvs-psql -e POSTGRES_PASSWORD=${db_pass} -e POSTGRES_USER=${db_user} -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+        exit $?
         ;;
     'start')
         echo 'start'
@@ -20,6 +40,7 @@ case $action in
         echo 'stop'
         ;;
     *)
-        echo 'invalid'
+        echo "Error: invalid action. $usage" >&2
+        exit 1
         ;;
 esac
