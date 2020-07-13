4c671ab1b52a2c9cb52659664ed75a339ca36e8b
Oscar Liang <sam.oscar.liang@gmail.com>
2020-07-13 18:37:35 +0000

    Completed SQL queries

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -5,3 +5,32 @@ FROM
   host_info
 ORDER BY 
   cpu_number ASC, total_mem DESC;
+
+--Query to find the average used memory in a 5-minute period
+SELECT 
+  id,
+  hostname,
+  date_trunc('hour', u.timestamp) + date_part('minute', u.timestamp)::int / 5 * interval '5 min' AS interval,
+  AVG((total_mem - memory_free) * 100 / total_mem)::int AS percentage_used
+FROM
+  host_info i JOIN
+    host_usage u ON
+    i.id = u.host_id
+GROUP BY
+  id,
+  interval
+ORDER BY
+  id,
+  interval;
+
+--Query to detect server failure, defined as outputting less than 3 data points in a 5-minute interval
+SELECT
+  host_id,
+  date_trunc('hour', timestamp) + date_part('minute', timestamp)::int / 5 * interval '5 min' AS ts,
+  COUNT(*) as num_data_points
+FROM
+  host_usage
+GROUP BY
+  host_id, ts
+HAVING
+  COUNT(*) < 3;
