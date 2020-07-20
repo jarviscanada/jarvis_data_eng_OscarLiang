9280bf76d6bc866105cb78b30ce2c00e73f9fb96
Oscar Liang <sam.oscar.liang@gmail.com>
2020-07-20 14:47:56 +0000

    Formatted code to follow Google style guide

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -4,28 +4,28 @@ import java.util.regex.*;
 
 public class RegexExcImp implements RegexExc {
 
-    private static String jpegRegex = "^\\S+\\.jpe?g$";
-    private static String ipRegex = "^(\\d?\\d?\\d\\.){3}\\d?\\d?\\d$";
-    private static String emptyRegex = "^\\s*$";
+  private static String jpegRegex = "^\\S+\\.jpe?g$";
+  private static String ipRegex = "^(\\d?\\d?\\d\\.){3}\\d?\\d?\\d$";
+  private static String emptyRegex = "^\\s*$";
 
-    @Override
-    public boolean matchJpeg(String filename) {
-        return matchRegex(jpegRegex, filename.toLowerCase());
-    }
+  @Override
+  public boolean matchJpeg(String filename) {
+    return matchRegex(jpegRegex, filename.toLowerCase());
+  }
 
-    @Override
-    public boolean matchIp(String ip) {
-        return matchRegex(ipRegex, ip);
-    }
+  @Override
+  public boolean matchIp(String ip) {
+    return matchRegex(ipRegex, ip);
+  }
 
-    @Override
-    public boolean isEmptyLine(String line) {
-        return matchRegex(emptyRegex, line);
-    }
+  @Override
+  public boolean isEmptyLine(String line) {
+    return matchRegex(emptyRegex, line);
+  }
 
-    private boolean matchRegex(String regex, String testString) {
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(testString);
-        return m.matches();
-    }
+  private boolean matchRegex(String regex, String testString) {
+    Pattern p = Pattern.compile(regex);
+    Matcher m = p.matcher(testString);
+    return m.matches();
+  }
 }
