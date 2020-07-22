4537c264ecd2d7c498fafee770b6f0028c7b0086
Oscar Liang <sam.oscar.liang@gmail.com>
2020-07-22 18:24:23 +0000

    Removed HelloWorld.java, added main function to RegexExcImp.java

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -1,5 +1,6 @@
 package ca.jrvs.apps.practice;
 
+import java.util.function.Function;
 import java.util.regex.*;
 
 public class RegexExcImp implements RegexExc {
@@ -7,6 +8,52 @@ public class RegexExcImp implements RegexExc {
     private static String jpegRegex = "^\\S+\\.jpe?g$";
     private static String ipRegex = "^(\\d?\\d?\\d\\.){3}\\d?\\d?\\d$";
     private static String emptyRegex = "^\\s*$";
+
+    public static void main(String[] args) {
+        String jpegString = "picture.jpeg";
+        String badJpegString = ".jpg";
+        String ipString = "127.0.0.1";
+        String badIpString = "1234.0.2.1";
+        String emptyString = "";
+        String badEmptyString = ".";
+        RegexExcImp regex = new RegexExcImp();
+        System.out.print(jpegString + ": ");
+        if (regex.matchJpeg(jpegString)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No match");
+        }
+        System.out.print(badJpegString + ":");
+        if (regex.matchJpeg(badJpegString)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No match");
+        }
+        System.out.print(ipString + ":");
+        if (regex.matchIp(ipString)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No match");
+        }
+        System.out.print(badIpString + ":");
+        if (regex.matchIp(badIpString)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No match");
+        }
+        System.out.print(emptyString + ":");
+        if (regex.isEmptyLine(emptyString)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No match");
+        }
+        System.out.print(badEmptyString + ":");
+        if (regex.isEmptyLine(badEmptyString)) {
+            System.out.println("Match");
+        } else {
+            System.out.println("No match");
+        }
+    }
 
     @Override
     public boolean matchJpeg(String filename) {
