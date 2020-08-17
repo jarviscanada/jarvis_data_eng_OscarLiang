d2c774bf75742020ea83175875706864e01def83
Oscar Liang <sam.oscar.liang@gmail.com>
2020-08-17 14:31:20 +0000

    Completed odd/even coding challenge

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/OddEven.java
@@ -0,0 +1,23 @@
+package ca.jrvs.practice.codingChallenge;
+
+/**
+ * ticket: https://www.notion.so/Sample-Check-if-a-number-is-even-or-odd-356af6834af94e4196e0e990ea6f8c7f
+ */
+public class OddEven {
+
+  /**
+   * Time complexity: O(1)
+   * Justification: Arithmetic operation
+   */
+  public String oddEvenMod(int i) {
+    return i % 2 == 0 ? "even" : "odd";
+  }
+
+  /**
+   * Time complexity: O(1)
+   * Justification: Function uses a single bit operation
+   */
+  public String oddEvenBit(int i) {
+    return (i & 1) == 0 ? "even" : "odd";
+  }
+}
