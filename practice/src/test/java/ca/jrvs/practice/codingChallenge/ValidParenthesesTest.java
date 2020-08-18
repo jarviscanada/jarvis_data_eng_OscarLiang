dbe4a597862e6c1b3d91271b4c886b77a3f42c8d
Oscar Liang <sam.oscar.liang@gmail.com>
2020-08-18 16:07:08 +0000

    Completed Parentheses coding challenge

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/ValidParenthesesTest.java
@@ -0,0 +1,20 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ValidParenthesesTest {
+
+  @Test
+  public void isValid() {
+    ValidParentheses vp = new ValidParentheses();
+    assertTrue(vp.isValid("()"));
+    assertTrue(vp.isValid("()[]{}"));
+    assertFalse(vp.isValid("(]"));
+    assertFalse(vp.isValid("([)]"));
+    assertTrue(vp.isValid("{[]}"));
+    assertFalse(vp.isValid("{"));
+    assertTrue(vp.isValid(""));
+  }
+}
