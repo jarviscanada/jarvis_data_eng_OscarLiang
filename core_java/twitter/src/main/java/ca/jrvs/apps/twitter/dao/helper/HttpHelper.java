57ce4fae712950a237726fd8d930ab3037bda0b5
Oscar Liang <sam.oscar.liang@gmail.com>
2020-08-28 18:01:54 +0000

    Added skeleton code for twitter app

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
@@ -0,0 +1,24 @@
+package ca.jrvs.apps.twitter.dao.helper;
+
+import java.net.URI;
+import org.apache.http.HttpResponse;
+import org.apache.http.entity.StringEntity;
+
+public interface HttpHelper {
+
+  /**
+   * Execute an HTTP POST call
+   *
+   * @param uri request uri
+   * @return response
+   */
+  HttpResponse httpPost(URI uri);
+
+  /**
+   * Execute an HTTP GET call
+   *
+   * @param uri request uri
+   * @return response
+   */
+  HttpResponse httpGet(URI uri);
+}
