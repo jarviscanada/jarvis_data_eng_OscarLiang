c32a631a54f69c40e56771477d5ae7c5df09d5dd
Oscar Liang <sam.oscar.liang@gmail.com>
2020-09-04 18:12:04 +0000

    Finish Api Test

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
@@ -6,7 +6,6 @@ import oauth.signpost.OAuthConsumer;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
