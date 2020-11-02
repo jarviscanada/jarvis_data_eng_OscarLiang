9381d4f43614bd1752b86533e8db48d4c2fab331
Oscar Liang <sam.oscar.liang@gmail.com>
2020-11-02 16:43:28 +0000

    Completed Two Sum Coding Challenge

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSum.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSum.java
@@ -6,6 +6,10 @@ import java.util.Map;
 
 public class TwoSum {
 
+  /**
+   * Time complexity: O(n^2)
+   * Brute force method uses a nested for loop to iterate through all elements in O(n^2) time.
+   */
   public int[] twoSumBruteForce(int[] nums, int target) {
     for (int i = 0; i < nums.length; i++) {
       for (int j = i; j < nums.length; j++) {
@@ -17,11 +21,12 @@ public class TwoSum {
     return null;
   }
 
+  /**
+   * Time complexity: O(n + nlogn)
+   * Assume that the Java sorting method takes O(nlogn) time. Then, after the array has been sorted,
+   * Iterate through the loop from both ends only once in O(n) time.
+   */
   public boolean twoSumSorted(int[] nums, int target) {
-    Map<Integer, Integer> originalIndices = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-      originalIndices.put(nums[i], i);
-    }
     Arrays.sort(nums);
     int leftIndex = 0;
     int rightIndex = nums.length - 1;
@@ -40,6 +45,12 @@ public class TwoSum {
     return false;
   }
 
+  /**
+   * Time complexity: O(n)
+   * Space complexity: O(n)
+   * Store the array in a map, which is O(N) space. Iterate through the array twice in O(n) time
+   * each.
+   */
   public int[] twoSumLinear(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
