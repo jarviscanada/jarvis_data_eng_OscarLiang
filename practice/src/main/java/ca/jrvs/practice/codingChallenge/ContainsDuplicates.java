544b30740cefa16523e5ddc01d229779273e5995
Oscar Liang <sam.oscar.liang@gmail.com>
2021-02-18 17:36:41 +0000

    Completed Contains Duplicates coding challenge

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/ContainsDuplicates.java
@@ -0,0 +1,42 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ticket: https://www.notion.so/Contains-Duplicate-a6a1aea7139e4b7b900784b2ff0f784b
+ */
+public class ContainsDuplicates {
+
+  /**
+   * Time complexity: O(n^2) - double for loop iterating through array in O(n^2) time.
+   * Space complexity: O(1) - constant sized variables
+   */
+  public boolean containsDuplicates(int[] nums) {
+    for (int i = 0; i < nums.length; i++) {
+      int numToCheck = nums[i];
+      for (int j = i + 1; j < nums.length; j++) {
+        if (numToCheck == nums[j]) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Time complexity: O(n) - iterate through array in linear time.
+   * Space complexity: O(n) - store single copy of values in hash table
+   */
+  public boolean containsDuplicatesSet(int[] nums) {
+    Set<Integer> uniqueValues = new HashSet<>();
+    for (int num : nums) {
+      if (uniqueValues.contains(num)) {
+        return true;
+      } else {
+        uniqueValues.add(num);
+      }
+    }
+    return false;
+  }
+}
---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/ContainsDuplicatesTest.java
@@ -0,0 +1,25 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContainsDuplicatesTest {
+
+  ContainsDuplicates containsDuplicates = new ContainsDuplicates();
+  @Test
+  public void containsDuplicates() {
+    int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+    assertFalse(containsDuplicates.containsDuplicates(nums));
+    nums = new int[] {1, 2, 3, 4, 1};
+    assertTrue(containsDuplicates.containsDuplicates(nums));
+  }
+
+  @Test
+  public void containsDuplicatesSet() {
+    int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+    assertFalse(containsDuplicates.containsDuplicatesSet(nums));
+    nums = new int[] {1, 2, 3, 4, 1};
+    assertTrue(containsDuplicates.containsDuplicatesSet(nums));
+  }
+}
